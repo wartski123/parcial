8eff2e5865f722acb6a8bad6b86c833734efca82
Nicolas Davila <Nicolas Davila@DESKTOP-QRBBQCS>
2017-05-22 10:28:01 -0500

    nicolas davila: Organizar numero de mayor a menor

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -5,6 +5,8 @@
  */
 package javaapplication4;
 
+import javax.swing.JOptionPane;
+
 
 
 /**
@@ -17,11 +19,35 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-            
-        System.out.println("primer repositorio prueba 2 mamama");
-        System.out.println("primer repositorio prueba 2 mamama");
-        System.out.println("pepe lo mama bueno");
-        System.out.println("iuahsidubwdovnw");
+int a, b, c;
+        a = Integer.parseInt(JOptionPane.showInputDialog("ingrese un numero"));
+        b = Integer.parseInt(JOptionPane.showInputDialog("ingrese un numero"));
+        c = Integer.parseInt(JOptionPane.showInputDialog("ingrese un numero"));
+
+        if (a > b) {
+            if (a > c) {
+                if (b > c) {
+                    JOptionPane.showMessageDialog(null, a + "" + b + "" + "" + c);
+                } else {
+                    JOptionPane.showMessageDialog(null, a + "" + c + "" + "" + b);
+                }
+
+            } else {
+                JOptionPane.showMessageDialog(null, c + "" + a + "" + "" + b);
+            }
+
+        } else {
+            if (b > c) {
+                if (c > a) {
+                    JOptionPane.showMessageDialog(null, b + "" + c + "" + "" + a);
+                } else {
+                    JOptionPane.showMessageDialog(null, b + "" + a + "" + "" + c);
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, c + "" + b + "" + "" + a);
+            }
+        }
+
     }
     
     
